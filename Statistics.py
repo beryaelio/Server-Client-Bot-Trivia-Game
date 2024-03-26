b070b814b57c2d774a63086b48095645a5a7eb65
GuyDulberg <163866624+GuyDulberg@users.noreply.github.com>
2024-03-26 12:33:52 +0200

    Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -4,14 +4,15 @@ import re
 class Statistics:
     """A class that stores data for statistics calculations of the players' performance"""
     def __init__(self):
-        self.player_stats = {}
-        self.player_victories = {}
-        self.player_games_played = {}
+        self.player_stats = {}  # Dictionary to store player statistics
+        self.player_victories = {}  # Dictionary to store player victories
+        self.player_games_played = {}  # Dictionary to store number of games each player played
 
-    def extract_round_number(self, message):
+    def extract_round_number(self,message):
         round_number_match = re.search(r'Round (\d+)', message)
         if round_number_match:
             round_number = int(round_number_match.group(1))
             return round_number
         else:
             return None
+            
